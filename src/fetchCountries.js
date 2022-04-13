256812c6a42852348bd2659fa693aad8885301f5
Helen <dd040388kev@gmail.com>
2022-04-13 10:34:33 +0300

    +

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,11 +1,11 @@
 function fetchCountries(name) {
   return fetch(
     `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`,
-  ).then(responce => {
-    if (!responce.ok) {
-      throw new Error(responce.status);
+  ).then(response => {
+    if (!response.ok) {
+      throw new Error(response.status);
       }
-     return responce.json();
+     return response.json();
        
   });
 }
